98dc718f3f95d53d33d7e654b5f23ee810ffe695
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-04-18 21:47:18 +0100

    Revert "test"
    This reverts commit 679ac63edbc4cb822105940a57904f74523140ab.

--- test/Main.purs
+++ test/Main.purs
@@ -2,10 +2,9 @@ module Test.Main where
 
 import Prelude
 
-import App.EntryPoint (generateFlavourText)
 import Effect (Effect)
 import Effect.Class.Console (log)
 
 main :: Effect Unit
 main = do
-  log $ generateFlavourText 60.0 66.0
+  log "You should add some tests."
