76cbb4782967f13c12d9dc88ca449d8b49239f9f
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-04-21 16:22:56 +0100

    express

--- ws-broadcast-express.js
+++ ws-broadcast-express.js
@@ -5,14 +5,15 @@ const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
-const server = express()
-.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
+const server = express();
+server.use('/', express.static(__dirname + "./dist"));
+/*.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
 .get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/app2.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/app.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/particles2.js'))
-.get('/', (req,res) => res.sendFile(__dirname + '/dev/particles.js'))
-.listen(PORT, () => console.log(`Express listening on ${PORT}`));
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/particles.js'))*/
+server.listen(PORT, () => console.log(`Express listening on ${PORT}`));
 
 const wss = new WebSocket.Server({ server });
 
