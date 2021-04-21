65a8033fe5a960fcfe58da0af19735ce5e3e409a
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-04-21 14:17:15 +0100

    404 fix

--- ws-broadcast-express.js
+++ ws-broadcast-express.js
@@ -6,7 +6,7 @@ const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
 
 const server = express()
-.get('/', (req,res) => res.sendFile(__dirname + '/index.html'))
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
 .get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))
 .listen(PORT, () => console.log(`Express listening on ${PORT}`));
 
