95d506e1bf530538c50df20faaa12a3637650fb5
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-04-21 16:12:42 +0100

    Update ws-broadcast-express.js

--- ws-broadcast-express.js
+++ ws-broadcast-express.js
@@ -5,11 +5,11 @@ const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
-const server = express()
-.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
-.get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))
-.listen(PORT, () => console.log(`Express listening on ${PORT}`));
-
+const server = express();
+server.use('dist');
+server.listen(PORT, () => console.log(`Express listening on ${PORT}`));
+/*.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
+.get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))*/
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
