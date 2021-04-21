14d510e95a2253b2394b4fa959e45ea2a6b34cd5
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-04-21 16:15:55 +0100

    Update ws-broadcast-express.js

--- ws-broadcast-express.js
+++ ws-broadcast-express.js
@@ -5,11 +5,15 @@ const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
-const server = express();
-server.use('public/dist');
-server.listen(PORT, () => console.log(`Express listening on ${PORT}`));
-/*.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
-.get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))*/
+const server = express()
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/app2.js'))
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/app.js'))
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/particles2.js'))
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/particles.js'))
+.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
+.get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))
+.listen(PORT, () => console.log(`Express listening on ${PORT}`));
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
