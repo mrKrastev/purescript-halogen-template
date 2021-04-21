0467b10a0f4a98113d89a0bacae039b90f624807
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-04-21 17:29:33 +0100

    dist update

--- dist/particles2.19fc590f.js
+++ dist/particles2.19fc590f.js
@@ -1762,7 +1762,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59440" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60090" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- ws-broadcast-express.js
+++ ws-broadcast-express.js
@@ -6,16 +6,14 @@ const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
 
 const server = express();
-server.use('/', express.static(__dirname + "/dev"));
-server.get('/index.js', (req,res) => res.sendFile(__dirname + './index.js'))
+server.use('/', express.static(__dirname + "/dist"));
 /*.get('/', (req,res) => res.sendFile(__dirname + '/dev/index.html'))
-
+.get('/index.js', (req,res) => res.sendFile(__dirname + '/index.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/app2.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/app.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/particles2.js'))
 .get('/', (req,res) => res.sendFile(__dirname + '/dev/particles.js'))*/
 server.listen(PORT, () => console.log(`Express listening on ${PORT}`));
-
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
